0abc34c07f8271822aa400ae9d9306c9af1a6004
Oliver Šulc <1001@student.itgymnazium.cz>
2022-06-13 21:57:44 +0200

    init

---
+++ ToDoDan/Models/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoDan.Models
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ItemName { get; set; }
+    }
+}
---
+++ ToDoDan/Models/ToDoDanDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoDan.Models
+{
+    public class ToDoDanDbContext : DbContext
+    {
+        public ToDoDanDbContext(DbContextOptions<ToDoDanDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<List> Lists { get; set; }
+    }
+}
